f88b77f76bab55700b5c642b3b282ec580ecf02b
Nikita <tr_nikita@mail.ru>
2022-12-24 20:15:32 +0700

    add Dockerfile

--- server/App.js
+++ server/App.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 const chalk = require('chalk');
+const path = require('path');
 const routes = require('./routes');
 const cors = require('cors');
 
@@ -14,6 +15,16 @@ app.use(cors());
 app.use('/api', routes);
 
 const PORT = config.get('port') ?? 8080;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client')));
+
+    const indexPath = path.join(__dirname, 'client', 'index.html');
+
+    app.get('*', (req, res) => {
+        res.sendFile(indexPath);
+    });
+}
 
 async function start() {
     try {
